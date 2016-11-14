90a435df5fbc942bcd8cfebd4936c04fc3ffb808
Daniel Roberson <daniel@revenantlabs.net>
2016-11-13 19:06:18 -0800

    initial commit

---
+++ zsh.d/less-syntax-highlighting.zsh
@@ -0,0 +1,6 @@
+# less syntax highlighting
+# requires source-highlighting package
+# TODO: add check for Linux and FreeBSD
+export LESSOPEN="|/usr/local/Cellar/source-highlight/3.1.8_3/bin/src-hilite-lesspipe.sh %s"
+export LESS=" -R "
+
