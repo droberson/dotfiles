9e283bcaa7ed138c286d5a4f272f3f2a07f752cd
Daniel Roberson <daniel@planethacker.net>
2016-11-17 10:16:24 -0800

    initial commit

---
+++ zsh.d/surfraw.zsh
@@ -0,0 +1,11 @@
+# Surfraw aliases!
+if program_exists sr; then
+  alias google="sr google"
+  alias imdb="sr imdb"
+  alias youtube="sr youtube"
+  alias define="sr webster"
+  alias wikipedia="sr wikipedia"
+  alias acronym="sr acronym"
+  alias thesaurus="sr thesaurus"
+fi
+
