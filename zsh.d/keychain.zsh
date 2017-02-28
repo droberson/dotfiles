4addff0c0134175aaf5e1fbe9afeaba2975b3938
Daniel Roberson <daniel@planethacker.net>
2017-02-28 07:18:14 -0800

    commenting and check if key exists

--- zsh.d/keychain.zsh
+++ zsh.d/keychain.zsh
@@ -1,8 +1,13 @@
 # keychain integration
 
+# check if keychain is installed
 which keychain >/dev/null
+if [ $? -eq 1 ]; then
+	exit
+fi
 
-if [ $? -eq 0 ]; then
+# check if rsa key exists
+if [ -e ~/.ssh/id_rsa ]; then
 	keychain -Q -q ~/.ssh/id_rsa
 	source ~/.keychain/`hostname`-sh
 fi
