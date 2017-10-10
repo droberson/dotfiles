ffc839bd962497edb7709ffc5815d5b65673374d
Daniel Roberson <daniel@svsc-syslog.ccdoa.net>
2017-10-10 07:45:38 -0700

    added check if source-highlight exists

--- zsh.d/less-syntax-highlighting.zsh
+++ zsh.d/less-syntax-highlighting.zsh
@@ -1,10 +1,15 @@
 # less syntax highlighting
 # requires source-highlighting package
 
-if [ `uname` = "Linux" ]; then
-    export LESSOPEN="|/usr/share/source-highlight/src-hilite-lesspipe.sh %s"
-elif [ `uname` = "Darwin" ]; then
-    export LESSOPEN="|/usr/local/Cellar/source-highlight/3.1.8_3/bin/src-hilite-lesspipe.sh %s"
+
+if [ $(uname) = "Linux" ]; then
+    if [ -x "/usr/share/source-highlight/src-hilite-lesspipe.sh" ]; then
+        export LESSOPEN="|/usr/share/source-highlight/src-hilite-lesspipe.sh %s"
+    fi
+elif [ $(uname) = "Darwin" ]; then
+    if [ -x "/usr/local/Cellar/source-highlight/3.1.8_3/bin/src-hilite-lesspipe.sh" ]; then
+        export LESSOPEN="|/usr/local/Cellar/source-highlight/3.1.8_3/bin/src-hilite-lesspipe.sh %s"
+    fi
 fi
 
 export LESS=" -R "
