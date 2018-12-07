8c34fffbc867ddf086e9d3115f880428818fe17f
Daniel Roberson <daniel@planethacker.net>
2018-12-07 08:21:48 -0800

    add logging of CLI arguments

--- zsh.d/nmap.zsh
+++ zsh.d/nmap.zsh
@@ -5,6 +5,10 @@ if [ $? -eq 0 ]; then
     if [ ! -d $HOME/.nmap ]; then
         mkdir $HOME/.nmap
     fi
-    nmap () { $NMAP_BIN $@ |tee $HOME/.nmap/nmap-$(date +%F-%T.%N).log }
+    nmap () {
+        NMAP_LOGFILE=$HOME/.nmap/nmap-$(date +%F-%T.%N).log
+        echo "nmap $@\n\n" > $NMAP_LOGFILE
+        $NMAP_BIN $@ |tee -a $NMAP_LOGFILE
+    }
 fi
 
